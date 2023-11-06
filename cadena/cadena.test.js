faa4e7b1c336530f331c1ae6db7082b5356c7123
Einyel2205 <126474340+Einyel2205@users.noreply.github.com>
2023-11-06 16:00:35 -0500

    Update cadena.test.js

--- cadena/cadena.test.js
+++ cadena/cadena.test.js
@@ -1,5 +1,5 @@
 const cadena = require ('./cadena');
 
 test ('adds hola==Adios to equal false', () =>{
-    expect(cadena ('hola == Adios')).toBe(false);
+    expect(cadena 'hola == Adios')).toBe(false);
 })
